6898114b6f27149a07574cf22b6968c0963a74ae
Boaz Sender <boazsender@Boaz-Senders-MacBook.local>
2011-09-11 15:25:42 -0400

    made it look nicer, and implemented swap button

--- scripts/remixer.js
+++ scripts/remixer.js
@@ -12,12 +12,17 @@ jQuery(function($){
   $.event.props.push('dataTransfer');
 
   $('body')
-    .bind( 'dragenter dragover', false)
-    .bind( 'drop', function( e ) {
-      e.stopPropagation();
-      e.preventDefault();
-      droplocation = $(e.target);
+  .bind( 'dragenter dragover', false)
+  .bind( 'drop', function( e ) {
+    e.stopPropagation();
+    e.preventDefault();
+    droplocation = $(e.target);
+    
+    // Only let videos get dropped from the file system onto one of the bins
+    if( droplocation.attr('id') === 'bin1' || droplocation.attr('id') === 'bin2'){
+      // Only try to put file(s) in the bins if some have been dragged to the browser
       if( e.dataTransfer && e.dataTransfer.files ){
+  
         $.each( e.dataTransfer.files, function(index, file){
           var fileReader = new FileReader();
               fileReader.onload = (function(file) {
@@ -35,8 +40,10 @@ jQuery(function($){
               })(file);
           fileReader.readAsDataURL(file);
         });
+  
       }
-    });
+    }
+  });
 
   $('#audioSource, #videoSource').droppable({
   	drop: function( event, ui ) {
@@ -72,6 +79,17 @@ jQuery(function($){
     
     newAudio.play()
     newVideo.volume(0).play()
+    
+  });
+  
+  $('#swapVideos').click(function(){
+    console.log('asd')
+    var video = $('#videoSource video'),
+        audio = $('#audioSource video');
+    
+
+        video.appendTo('#audioSource')
+        audio.appendTo('#videoSource')
     
   });
   
